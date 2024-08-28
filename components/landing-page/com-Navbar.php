a411b0b0285530eb0e333bacdab134baa970feff
AliAmir9586 <ali8992022@gmail.com>
2024-08-28 21:12:16 +0500

    test navbar first commit

--- components/landing-page/com-Navbar.php
+++ components/landing-page/com-Navbar.php
@@ -32,7 +32,7 @@
                     data-bs-toggle="dropdown"
                     aria-expanded="false"
                   >
-                    Category
+                    My name is ali
                   </a>
                   <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="#">Action</a></li>
