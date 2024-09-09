f7242e14fbf765faaeabe2d1be931a0a87680243
AliAmir9586 <ali8992022@gmail.com>
2024-09-09 23:02:26 +0500

    cart button

--- cart-section.php
+++ cart-section.php
@@ -19,7 +19,7 @@
     include "./components/cart/com-cart.html"
     ?>
 
-    
+    <script src="./js/product.js"></script>
     <script src="./js/cart.js"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
